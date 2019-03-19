d499df4d75aeb45378bca68314b0ea13bd926b4b
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-19 15:14:52 -0300

    font awsome

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/brands.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/fontawesome.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/regular.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
