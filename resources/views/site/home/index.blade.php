9fc52d29e54fd2ed71bd5d146403e87eb07a62a0
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-19 14:33:51 -0300

    aos min

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/brands.min.css" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/fontawesome.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
@@ -19,7 +19,7 @@
 </body>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.min.js"></script>
-<script src="https://unpkg.com/aos@next/dist/aos.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" ></script>
 
 <script src="{{ mix('js/site.js') }}"></script>
 
