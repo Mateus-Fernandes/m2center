71e08e0d197f95edaa6603e6a053e95ba208a9cb
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-19 19:27:24 -0300

    captcha off

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -25,5 +25,7 @@
 <script src="{{ mix('js/site.js') }}"></script>
 
 <script src="{{ mix('js/custom.js') }}"></script>
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
+
+
 </html>
