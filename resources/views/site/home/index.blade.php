0689c71fba7ed28437dc96787bff49d6afe908be
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-20 13:37:05 -0300

    font awsome

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -4,6 +4,9 @@
     <title>M2</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/brands.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/fontawesome.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/regular.min.css" crossorigin="anonymous">    
     <link rel="stylesheet" media="none" onload="document.body.className+='exofont'; this.media='all';" type="text/css" href="{{ mix('css/style.css') }}">   
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
