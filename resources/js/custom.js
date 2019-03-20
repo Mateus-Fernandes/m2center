f874cc9a6c666cfd186600243b91ebc376f393a1
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-20 10:04:07 -0300

    remove jquery

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,44 +1,17 @@
 
 
 
-          import SVGInjector from 'svg-injector';
-          var mySVGsToInject = document.querySelectorAll('.injetar');
-          SVGInjector(mySVGsToInject);                    
-
-
+            
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 window.onload=function(){
-
   AOS.init();
-  document.addEventListener('aos:in:super-duper', () => {
-    //console.log(this.className);
-  });
-  
-// Animação dos botões principais 
-    var buttonEl = document.querySelectorAll('.botaoNovo');
-    function animateButton(width, duracao) {
-        anime.remove('rect');
-        anime({
-        targets: 'rect',
-        width: width,
-        easing: 'easeInOutSine',
-        endDelay: 1000,
-        duration: duracao,
-      }).finished;
-    }
-    function enterButton() { animateButton(300,2000) };
-    function leaveButton() { animateButton(3,50) };
-    for (var i = 0; i < buttonEl.length; i++) { 
-        buttonEl[i].addEventListener('mouseenter', enterButton, false);
-        buttonEl[i].addEventListener('mouseleave', leaveButton, false);
-    }
 // Animação no formulario do site
     let current = null;
     const nome = document.querySelector('#nome'),
-  email = document.querySelector('#email'),
-  telefone = document.querySelector('#telefone');
+    email = document.querySelector('#email'),
+    telefone = document.querySelector('#telefone');
 nome.addEventListener('focus', (e) => { 
     if (current) current.pause();
     current = anime({
@@ -83,13 +56,5 @@ mensagem.addEventListener('focus', (e) => {
           loop: false
     });
 });
-  // Animação do box
-anime({
-    targets: '.caixas',
-    backgroundSize: '100%',
-    easing: 'easeInOutSine',
-    duration: 10000,
-    direction: 'alternate',
-    loop: true
-  }).finished;
+
 }
--- resources/js/site.js
+++ resources/js/site.js
@@ -5,27 +5,10 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-window._ = require('lodash');
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-/**
- * Next we will register the CSRF Token as a common header with Axios so that
- * all outgoing HTTP requests automatically have it attached. This is just
- * a simple convenience so we don't have to attach every token manually.
- */
-
-let token = document.head.querySelector('meta[name="csrf-token"]');
-
-
-if (token) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+require('./bootstrap');
 
 window.Vue = require('vue');
+
 import VueRouter from 'vue-router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
