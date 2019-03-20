aa597495f2e4745ffdf0a130cd9d42d4617cd43e
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-20 09:48:21 -0300

    loadash

--- resources/js/site.js
+++ resources/js/site.js
@@ -5,15 +5,30 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+window._ = require('lodash');
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 window.Vue = require('vue');
-
 import VueRouter from 'vue-router';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
-import VModal from 'vue-js-modal';
-Vue.use(VModal);
 import FlashMessage from '@smartweb/vue-flash-message';
 Vue.use(FlashMessage);
 var VueScrollTo = require('vue-scrollto');
