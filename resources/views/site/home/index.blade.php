937e865b7fe07006813a04a397d8bb72bb52ae33
Mateus Fernandes <mateusfernandes12@live.com>
2019-03-20 10:23:40 -0300

    productions test

--- resources/views/site/home/index.blade.php
+++ resources/views/site/home/index.blade.php
@@ -5,12 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" type="text/css" href="{{ mix('css/style.css') }}">
-    <link href="https://fonts.googleapis.com/css?family=Exo:400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/brands.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/fontawesome.min.css" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/regular.min.css" crossorigin="anonymous">
-    
+    <link href="https://fonts.googleapis.com/css?family=Exo:400,700|Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <style>
